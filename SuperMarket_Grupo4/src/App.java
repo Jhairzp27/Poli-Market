442e6d455a6f6a7949926f423abf1e78e01e5e8e
Jhairzp_27 <jhairzp27@hotmail.com>
2023-12-21 13:08:56 -0500

    Control de nombre empresa y numero de telefono

--- SuperMarket_Grupo4/src/App.java
+++ SuperMarket_Grupo4/src/App.java
@@ -1,6 +1,39 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
         
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        Empresa nEmpresa = new Empresa("PoliMarket", "2420626", 547662);
+        // Modificacion del nombre de la empresa
+        nEmpresa.setNombreEmpresa("Poli Market");
+        // Modificacion del nombre de la empresa
+        nEmpresa.setNumTelefonico("453625457");
+        // Modificacion del nombre de la empresa
+        nEmpresa.setIdent(547892);
+
+
+        System.out.println("Bienvenido a: " + nEmpresa.getNombreEmpresa() + " s.a");
+        System.out.println("Numero telefonico: " + nEmpresa.getNumTelefonico());
+        System.out.println("Identificador: " + nEmpresa.getIdent());
+        // QUE PROCESO DESEA REALIZAR EJ: Comprar
+
+        // CREAR PROCESO PARA DETERMINAR QUE TIPO DE CLIENTE ES EL USUARIO
+
     }
 }
--- SuperMarket_Grupo4/src/Empresa.java
+++ SuperMarket_Grupo4/src/Empresa.java
@@ -1,3 +1,60 @@
+/**
+ * @author: Jhair Zambrano
+ */
 public class Empresa {
+
+    private String nombreEmpresa;
+    private String numTelefonico;
+    private int ident;
+
+    String red = "\033[31m", reset = "\u001B[0m";
+
+    
+    public Empresa(String nombreEmpresa, String numTelefonico, int ident){
+        this.nombreEmpresa = nombreEmpresa;
+        this.numTelefonico = numTelefonico;
+        this.ident = ident;
+    }
+    
+    //NOMBRE DE LA EMPRESA
+    public String getNombreEmpresa() {
+
+        return nombreEmpresa.toUpperCase();
+    }
+
+    public void setNombreEmpresa(String nombreEmpresa) {
+
+        if (nombreEmpresa.length() <= 13) {
+            this.nombreEmpresa = nombreEmpresa.toUpperCase();
+        } else {
+            System.out.println(red + "Su nombre se ha recortado a los primeros 13 caracteres permitidos por la SCVS" + reset);
+            this.nombreEmpresa = nombreEmpresa.substring(0, 12).toUpperCase();
+        }
+    }
+    
+    //NUMERO TELEFONICO
+    public String getNumTelefonico() {
+        return numTelefonico;
+    }
+
+    public void setNumTelefonico(String numTelefonico) {
+        if (numTelefonico.length() <= 8) {
+            this.numTelefonico = numTelefonico;
+        } else {
+            this.numTelefonico = numTelefonico.substring(0, 7);
+        }
+    }
+
+    //IDENTIFICADOR DE EMPRESA
+    public int getIdent() {
+        return ident;
+    }
+
+    public void setIdent(int ident) {
+        this.ident = ident;
+    }
+
+
+
 
 }
