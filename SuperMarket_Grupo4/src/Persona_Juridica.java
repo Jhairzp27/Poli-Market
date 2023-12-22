160fc365024814f007322d94d811729a06fa6668
Jhordyp36 <jhordyp2004@gmail.com>
2023-12-21 19:18:13 -0500

    Creacion Persona Juridica

--- SuperMarket_Grupo4/src/Persona_Juridica.java
+++ SuperMarket_Grupo4/src/Persona_Juridica.java
@@ -1,3 +1,37 @@
-public class Persona_Juridica {
+public class Persona_Juridica extends Cliente{
+    private String ruc;
+    private String tipoPersonaJuridica;
+    private String representanteLegal;
+    
+    public String getRepresentanteLegal() {
+        return representanteLegal;
+    }
+    public void setRepresentanteLegal(String representanteLegal) {
+        this.representanteLegal = representanteLegal;
+    }
+    public String getTipoPersonaJuridica() {
+        return tipoPersonaJuridica;
+    }
+    public void setTipoPersonaJuridica(String tipoPersonaJuridica) {
+        this.tipoPersonaJuridica = tipoPersonaJuridica;
+    }
+    public String getRuc() {
+        return ruc;
+    }
+    public void setRuc(String ruc) {
+        this.ruc = ruc;
+    }
 
+    public Persona_Juridica(String id, String nombre, String direccion, String nroTelefono, String eMail, 
+                            String ruc, String tipoPersonaJuridica, String representanteLegal){
+        super();
+        setId           (id);
+        setNombre       (nombre);
+        setDireccion    (direccion);
+        setNroTelefonico(nroTelefono);
+        seteMail        (eMail);
+        this.ruc =       ruc;
+        this.tipoPersonaJuridica = tipoPersonaJuridica;
+        this.representanteLegal =  representanteLegal;
+    }
 }
