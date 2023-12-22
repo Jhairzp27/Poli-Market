f55ae3d483f5163fb35f6ca944f6df352b69d3ed
fernygeek <yofer68@gmail.com>
2023-12-21 21:31:04 -0500

    updateUseCase

--- SuperMarket_Grupo4/src/Cajero.java
+++ SuperMarket_Grupo4/src/Cajero.java
@@ -11,6 +11,62 @@ public class Cajero {
     private int numeroDeCaja;
     private int ventasRealizadas;
     private boolean estadoDeCaja;
-    
+
     public Empresa e;
+
+    public int getIdCajero() {
+        return idCajero;
+    }
+
+    public void setIdCajero(int idCajero) {
+        this.idCajero = idCajero;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getTurnoDeTrabajo() {
+        return turnoDeTrabajo;
+    }
+
+    public void setTurnoDeTrabajo(String turnoDeTrabajo) {
+        this.turnoDeTrabajo = turnoDeTrabajo;
+    }
+
+    public int getNumeroDeCaja() {
+        return numeroDeCaja;
+    }
+
+    public void setNumeroDeCaja(int numeroDeCaja) {
+        this.numeroDeCaja = numeroDeCaja;
+    }
+
+    public int getVentasRealizadas() {
+        return ventasRealizadas;
+    }
+
+    public void setVentasRealizadas(int ventasRealizadas) {
+        this.ventasRealizadas = ventasRealizadas;
+    }
+
+    public boolean isEstadoDeCaja() {
+        return estadoDeCaja;
+    }
+
+    public void setEstadoDeCaja(boolean estadoDeCaja) {
+        this.estadoDeCaja = estadoDeCaja;
+    }
 }
